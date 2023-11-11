16c4fd2aa5d73b0a0d88b101b94f3647bfbde330
Frank Villachica <furapay@gmail.com>
2023-11-11 16:15:48 -0500

    Last update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,25 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), {
-    name: "babel",
-    config: {
-      "presets": ["@babel/preset-env"]
-    }
-  }, react()]
+  integrations: [
+    tailwind(),
+    {
+      name: "babel",
+      config: {
+        presets: ["@babel/preset-env"],
+      },
+    },
+    react(),
+  ],
+  vite: {
+    resolve: {
+      alias: {
+        "@layouts/MainLayout": "@/layouts/MainLayout.js",
+      },
+    },
+  },
 });
